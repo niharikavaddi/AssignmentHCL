48abc90961e08486f15e7d4324ccad3715eeb900
VADDI LAKSHMI NEEHARIKA <36045615+niharikavaddi@users.noreply.github.com>
2020-07-30 18:23:36 +0530

    Create display.java
    To display the last 5 transactions and account balance

---
+++ javafiles/display.java
@@ -0,0 +1,46 @@
+package Pack;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet("/Display")
+public class Display extends HttpServlet {
+	public void doPost(HttpServletRequest request,HttpServletResponse response) throws IOException{
+		PrintWriter out= response.getWriter();
+		Connection con=null;
+		PreparedStatement psmt=null;
+		ResultSet rs=null;
+		String url="jdbc:mysql://127.0.0.1:3306/test?autoReconnect=true&useSSL=false",user="niharika",password="abcdefg";
+		try
+		{
+		Class.forName("com.mysql.jdbc.Driver");
+	    con=DriverManager.getConnection(url,user,password);
+	    psmt=con.prepareStatement("select amount from accountdetails where accID=23456782");
+	    rs=psmt.executeQuery();
+	    if(rs.next())
+	    out.println("ACCOUNT BALANCE:"+rs.getString(1));
+	    psmt=con.prepareStatement("select * from transactions order by id desc limit 5;");
+	    rs=psmt.executeQuery();
+	    int i=1;
+	    while(rs.next())
+		{
+	    	out.println(i+". "+rs.getString(1));
+	    	i++;
+		}
+	    rs.close();
+	    psmt.close();
+		con.close();
+		}
+		catch(Exception e)
+		{
+			out.println(e.toString());
+		}
+
+	}
+}
